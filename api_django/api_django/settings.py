8eb868e0d7f4c2df61ef0a301fb047d6b07ff571
RubenMkda <Moncadaruben22@gmail.com>
2023-08-08 08:25:33 -0400

    preparing for deployment

--- api_django/api_django/settings.py
+++ api_django/api_django/settings.py
@@ -82,6 +82,13 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.memcached.PyMemcacheCache",
+        "LOCATION": "127.0.0.1:11211",
+    }
+}
+
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'users.backends.CustomUserModelBackend',
