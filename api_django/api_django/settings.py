34e7bd2c358519fc871421e688178fdfe35f64d1
RubenMkda <Moncadaruben22@gmail.com>
2023-08-08 08:05:53 -0400

    preparing for deployment

--- api_django/api_django/settings.py
+++ api_django/api_django/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://django-project-6s01-dev.fl0.io/']
 
 
 # Application definition
